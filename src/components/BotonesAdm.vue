3492c1697f35137a3f0b31dc40f1d08f5e57a48e
unknown <geoffreyytorres25@gmail.com>
2020-10-27 09:08:34 -0600

    X_YY

--- src/components/BotonesAdm.vue
+++ src/components/BotonesAdm.vue
@@ -4,19 +4,19 @@
       <div class>
         <div class="flex">
           <div class="border-black border-2 rounded-lg m-8 sm:m-4 sm:p-3 shadow-xl">
-            <router-link to="/NuevoDtc" class="text-center pointer">
+            <router-link to="/NuevoDtc" class="text-center cursor-pointer">
               <img src="../assets/img/nuevoDtc.png" height="200" width="200" class="m-10 sm:m-1" />
               <h1 class="text-gray-900 text-xl sm:text-sm font-black font-mono">Nuevo DTC</h1>
             </router-link>
           </div>
           <div class="border-black border-2 rounded-lg m-8 sm:m-4 sm:p-3 shadow-xl">
-            <router-link to="/NuevoDTCLibre" class="text-center">
+            <router-link to="/NuevoDTCLibre" class="text-center cursor-pointer">
               <img src="../assets/img/dtcLibre.png" height="200" width="200" class="m-10 sm:m-1" />
               <h1 class="text-gray-900 text-xl sm:text-sm font-black font-mono">Nuevo DTC Libre</h1>
             </router-link>
           </div>
           <div class="border-black border-2 rounded-lg m-8 sm:m-4 sm:p-3 shadow-xl">
-            <router-link to="/ListarDTC" class="text-center">
+            <router-link to="/ListarDTC" class="text-center cursor-pointer">
               <img src="../assets/img/perfiles.png" height="200" width="200" class="m-10 sm:m-1" />
               <h1 class="text-gray-900 text-xl sm:text-sm font-black font-mono">Revisar DTC</h1>
             </router-link>
@@ -24,13 +24,13 @@
         </div>
         <div class="flex">
           <div class="border-black border-2 rounded-lg m-8 sm:m-4 sm:p-3 shadow-xl">
-            <router-link to="/Inventario" class="text-center">
+            <router-link to="/Inventario" class="text-center cursor-pointer">
               <img src="../assets/img/inventario.png" height="200" width="200" class="m-10 sm:m-1" />
               <h1 class="text-gray-900 text-xl sm:text-sm font-black font-mono">Inventario Plaza</h1>
             </router-link>
           </div>
           <div class="border-black border-2 rounded-lg m-8 sm:m-4 sm:p-3 shadow-xl">
-            <router-link to="/Configuracion" class="text-center">
+            <router-link to="/Configuracion" class="text-center cursor-pointer">
               <img src="../assets/img/config.png" height="200" width="200" class="m-10 sm:m-1" />
               <h1 class="text-gray-900 text-xl sm:text-sm font-black font-mono">Configuración</h1>
             </router-link>
