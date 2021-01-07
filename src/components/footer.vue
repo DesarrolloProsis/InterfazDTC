90d379f42558ae18c4fca1cee5f96afeb1babd31
unknown <geoffreyytorres25@gmail.com>
2021-01-07 10:19:50 -0600

    Footer-Responsive

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,14 +1,14 @@
 <template>
     <footer class="relative">        
-            <div class="absolute sm:container w-full px-6 bg-blue-900">                
-                    <div class="sticky inset-x-0 bottom-0 inline-flex justify-between sm:w-2/3 py-3 w-full">
-                        <div class="w-full ml-16 text-center">
+            <div class="absolute w-full px-6 bg-blue-900">                
+                    <div class="sticky inset-x-0 bottom-0 inline-flex justify-between  py-3 w-full">
+                        <div class="w-full ml-16 sm:ml-0 text-center">
                             <p class="text-sm text-white">
                                 © 2021 Soporte y Servicios Informaticos.
                             </p>
                         </div>
                         <div>
-                            <p class=" text-blue-400">V1.1.1</p>
+                            <p class="sm:text-xs text-sm sm:mt-1 text-blue-400">V1.1.2</p>
                         </div>
                     </div>                                
             </div>
