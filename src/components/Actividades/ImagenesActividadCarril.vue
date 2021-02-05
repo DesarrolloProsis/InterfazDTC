0ee284055894b53085ac5e36dcc9124429d6d1c6
AXEL FRIAS HERNANDEZ <axel.frias257@gmail.com>
2021-02-05 11:15:21 -0600

    Avances-Comp-IMG

--- src/components/Actividades/ImagenesActividadCarril.vue
+++ src/components/Actividades/ImagenesActividadCarril.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
         <p class="text-center font-bold text-xl text-gray-800 mb-5">Imagenes</p>          
-        <div class=" inline-flex h-40 w-full">
-            <div class="w-2/3">
-                <img src="../../assets/img/add.png" class="h-40" />
+        <div class=" inline-flex h-40 border border-gray-800 w-full">
+            <div class="w-2/3 grid grid-cols-3 gap-4 overflow-auto">
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+                <img src="../../assets/img/add.png" class="" />
+
             </div>
-            <div class="w-1/3 border-red-400 border">
+            <div class="w-1/3 border-2 border-gray-600 border-dashed text-center">
                 <input type="file" class=" h-40 w-32 opacity-0 absolute">
-                <img src="../../assets/img/image-mini.png" class="w-32 mr-3 mt-3 border" alt/>
+                <img src="../../assets/img/more.png" class="w-full h-32 p-5 m t-3" alt/>
+                <span class=" text-gray-500 text-sm">Agregar imagenes</span>
             </div>
         </div>
     </div>
