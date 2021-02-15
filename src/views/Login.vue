529d620e71d6d31ab8456076772bf64dbdc0763a
AXEL FRIAS HERNANDEZ <axel.frias257@gmail.com>
2021-02-15 13:48:07 -0600

    Icono-Ver-Pass

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,43 +14,27 @@
         <br />
         <div class="mt-10">
           <div class="mb-5">
-            <input 
-              v-validate="'required'" 
-              v-model="datos.User"
-              class="w-full h-8" 
-              type="text" 
-              name="Usuario" 
-              :class="{ is_valid: !errors.first('Usuario'),is_invalid: errors.first('Usuario')}" 
-              placeholder="  Usuario" 
-            />
+            <input  v-validate="'required'" v-model="datos.User" class="w-full h-8" type="text" name="Usuario" :class="{ is_valid: !errors.first('Usuario'),is_invalid: errors.first('Usuario')}" placeholder="  Usuario" />
             <span class="text-red-600 text-xs">{{ errors.first("Usuario") }}</span>
           </div>
           <div class="mb-5">
-            <input
-              @keyup.enter="ingresarLogin()"
-              placeholder=" Contraseña"
-              class="w-full h-8"
-              v-validate="'required'"
-              :class="{ is_valid: !errors.first('Contraseña'),is_invalid: errors.first('Contraseña')}"
-              type="password"
-              name="Contraseña"
-              v-model="datos.Password"
-            />
+            <div class="w-full inline-flex relative">
+              <input @keyup.enter="ingresarLogin()" placeholder=" Contraseña" class="w-full h-8" v-validate="'required'" :class="{ is_valid: !errors.first('Contraseña'),is_invalid: errors.first('Contraseña')}" :type="tipoInput" name="Contraseña" v-model="datos.Password" />
+              <span class="absolute right-0 mt-2 mr-2 cursor-pointer" @click="tipoInput == 'password' ? tipoInput = 'text' : tipoInput = 'password'">
+                <img v-if="tipoInput == 'password'" src="../assets/img/visibility.png" class="w-5" />
+                <img v-else src="../assets/img/notvisibility.png" class="w-5" />
+              </span>
+            </div>
             <span class="text-red-600 text-xs">{{ errors.first("Contraseña") }}</span>
           </div>
+          
         </div>
         <div class="text-center text-gray-900">
           <input v-model.number="datos.checkLog" class="mt-10 mb-10 mr-2" type="checkbox"/>
           <span>Generar Por Otra Persona</span>
         </div>
         <div class="container-login100-form-btn">
-          <button
-            @click="ingresarLogin()"
-            type="button"
-            class="login100-form-btn text-blue-600 outline-none"
-          >
-            Login
-          </button>
+          <button @click="ingresarLogin()" type="button" class="login100-form-btn text-blue-600 outline-none">Login</button>
         </div>
         <div class="flex flex-col text-center mt-3 text-blue-700">
           <a class="hover:text-blue-900 cursor-pointer" @click="register">Registrarse</a>
@@ -69,59 +53,22 @@
       </div>
       <div class="mb-4">
         <p class="text-xs mb-1 text-gray-900">Plaza</p>
-        <select
-          @change="buscarTec()"
-          v-validate="'required'"
-          :class="{ is_valid: !errors.first('Plaza'),is_invalid: errors.first('Plaza')}"
-          v-model="plazaSelect"
-          class="w-48 h-8"
-          type="text"
-          name="Plaza"
-        >
+        <select @change="buscarTec()" v-validate="'required'" :class="{ is_valid: !errors.first('Plaza'),is_invalid: errors.first('Plaza')}" v-model="plazaSelect" class="w-48 h-8" type="text" name="Plaza">
           <option value>Selecionar...</option>
-          <option
-            v-for="(item, index) in listaPlazas"
-            v-bind:value="item.squareCatalogId"
-            :key="index"
-          >
-            {{ item.squareName }}
-          </option>
+          <option v-for="(item, index) in listaPlazas" v-bind:value="item.squareCatalogId" :key="index">{{ item.squareName }}</option>
         </select>
         <p class="text-red-600 text-xs">{{ errors.first("Plaza") }}</p>
       </div>
       <div class="mb-8">
         <p class="text-xs mb-1 text-gray-900">Tecnico</p>
-        <select
-          v-model="tecSelect"
-          v-validate="'required'"
-          class="h-8 w-48"
-          :class="{ is_valid: !errors.first('Tecnico'), is_invalid: errors.first('Tecnico') }"
-          type="text"
-          name="Tecnico"
-        >
+        <select v-model="tecSelect" v-validate="'required'" class="h-8 w-48" :class="{ is_valid: !errors.first('Tecnico'), is_invalid: errors.first('Tecnico') }" type="text" name="Tecnico">
           <option value>Selecionar...</option>
-          <option
-            v-for="(item, index) in listaTec"
-            v-bind:value="item.userId"
-            :key="index"
-          >
-            {{ item.tecnicosAsignados }}
-          </option>
+          <option v-for="(item, index) in listaTec" v-bind:value="item.userId" :key="index">{{ item.tecnicosAsignados }}</option>
         </select>
         <p class="text-red-600 text-xs">{{ errors.first("Tecnico") }}</p>
       </div>
-      <button
-        @click="loginOther()"
-        class="text-white mb-5 px-4 py-2 rounded-full bg-blue-800"
-      >
-        Ingresar
-      </button>
-      <button
-        @click="modal = false"
-        class="text-white px-4 py-2 rounded-full bg-red-800"
-      >
-        Cancelar
-      </button>
+      <button @click="loginOther()" class="text-white mb-5 px-4 py-2 rounded-full bg-blue-800">Ingresar</button>
+      <button @click="modal = false" class="text-white px-4 py-2 rounded-full bg-red-800">Cancelar</button>
     </div>
   </div>
 </template>
@@ -141,6 +88,7 @@ export default {
       listaTec: [],
       plazaSelect: "",
       tecSelect: "",
+      tipoInput : 'password'
     };
   },
 /////////////////////////////////////////////////////////////////////
