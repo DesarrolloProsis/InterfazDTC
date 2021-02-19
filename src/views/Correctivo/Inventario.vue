9da18edb01502513a8b803bac31fc3859e43352e
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-02-19 14:57:37 -0600

    Cambio botones Carrusel

--- src/views/Correctivo/Inventario.vue
+++ src/views/Correctivo/Inventario.vue
@@ -4,12 +4,12 @@
     <div class="flex justify-center">
       <div class="grid gap-4 grid-cols-1 py-3 px-3">
         <!--<Generico :titulo="'INVENTARIO'" :tipo="'INV'"></Generico>-->
-        <h1 class="text-black text-center text-4xl mt-3 mb-1 sm:mb-1 sm:text-2xl font-bold">Inventario</h1>
         <!--/////////////////////////////////////////////////////////////////
         ////                     BOTONES NAVEGACION                      ////
         ////////////////////////////////////////////////////////////////////-->
         <div class="mt-1 mb-1 justify-center sm:block sm:p-1 sm:pr-2 border sm:m-1 shadow-md grid grid-cols">
-          <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 mt-2 sm:text-xs sm:ml-3">
+          <h1 class="text-black text-center text-4xl mt-3 sm:mb-1 sm:text-2xl font-bold">Inventario</h1>
+          <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 mt-1 sm:text-xs sm:ml-3">
             <div class="mr-3 sm:mr-1 mt-6">
               <span class="mr-2">Buscar</span>
               <input v-model="buscar_palabra" class="bg-white sm:w-full" />
