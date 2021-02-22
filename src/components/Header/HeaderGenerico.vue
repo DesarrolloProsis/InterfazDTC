fc07205b2b6be2e0d2cd6ff367d5f622e3596b77
unknown <geoffreyytorres25@gmail.com>
2021-02-22 10:50:46 -0600

    Bug-Titulo

--- src/components/Header/HeaderGenerico.vue
+++ src/components/Header/HeaderGenerico.vue
@@ -60,11 +60,7 @@
             </div>
         </div>
     <!-- ////////////////////////////////////////////////////////////////////
-<<<<<<< HEAD
         ///                         BOTONES inventario               ////
-=======
-        ///                BOTONES DE NAVEGACION INVENTARIO             ////
->>>>>>> ba112e1a704990e7f6ad4d83f279318ea4a25ed2
        ////////////////////////////////////////////////////////////////////-->
         <div class="mb-3 text-center sm:mt-3 sm:mb-4 sm:ml-4 sm:text-xs mt-5 mr-5 sm:inline-flex">
             <button @click="cancelar_filtro_inventario" class="w-32 botonIconBorrarCard ml-4 mr-4">
@@ -80,9 +76,9 @@
     <!--///////////////////////////////////////////////////////////////////
        ///         FILTROS DE NAVEGACION CALENDARIO                   ////         
      ///////////////////////////////////////////////////////////////////-->
-    <div class="inline-flex sm:inline-block w-full">
+    <div v-if="tipo == 'CAL'" class="inline-flex sm:inline-block w-full">
         <h1 class="text-black text-center text-4xl mt-3 mb-1 sm:mb-1 sm:text-2xl font-bold">{{ titulo }}</h1>
-        <div v-if="tipo == 'CAL'" class="sm:w-full inline-flex sm:inline-block text-base sm:text-sm">
+        <div class="sm:w-full inline-flex sm:inline-block text-base sm:text-sm">
             <div class="sm:w-full ml-5">
                 <div class="md:flex lg:flex xl:flex justify-start sm:grid-cols-1">
                     <p class="font-bold">Plaza de Cobro:</p>
