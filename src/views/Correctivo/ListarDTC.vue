dcea730ab204159787dac6ff37979fc92629926b
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-02-23 11:37:52 -0600

    Botones ListarDTC

--- src/views/Correctivo/ListarDTC.vue
+++ src/views/Correctivo/ListarDTC.vue
@@ -142,12 +142,12 @@
         ////                      MODAL CONFIRMAR GMMEP                 ////
         ////////////////////////////////////////////////////////////////////-->
         <div class="sticky inset-0">
-        <div v-if="modalFirma" class="rounded-lg  justify-center border absolute inset-x-0 bg-white border-gray-700 w-69 mx-auto px-12 py-10 shadow-2xl">
-          <div class="rounded-lg border bg-white border-gray-700 px-12 py-10 shadow-2xl">
+        <div v-if="modalFirma" class="rounded-lg justify-center border absolute inset-x-0 bg-white border-gray-700 w-69 mx-auto px-12 py-10 shadow-2xl">
+          <div class=" bg-white px-12 py-10">
             <p class="text-gray-900 font-thin text-md">Seguro que quieres agregar autorizacion GMMEP a este DTC {{ refNum }}</p>
-            <div class="justify-center flex mt-5">
+            <div class="mt-5 justify-items-center">
               <button @click="agregar_autorizacion_gmmep(true)" class="botonIconCrear">Si</button>
-              <button @click="agregar_autorizacion_gmmep(false)" class="botonIconBorrarCard">No</button>
+              <button @click="agregar_autorizacion_gmmep(false)" class="botonIconCancelar">No</button>
             </div>
           </div>
         </div>
@@ -158,7 +158,7 @@
         <div class="sticky inset-0">
         <div v-if="modalEliminar" class="rounded-lg  justify-center border absolute inset-x-0 bg-white border-gray-700 w-69 sm:w-64 mx-auto px-12 py-10 shadow-2xl">
           <p class="text-gray-900 font-thin text-md sm:text-sm sm:text-center">Seguro que quiere eliminar este DTC {{ refNum }}</p>
-          <div class="justify-center flex mt-5">
+          <div class="flex mt-5">
             <button @click="borrar(true)" class="botonIconCrear">Si</button>
             <button @click="(modal = modalEliminar = false), (refNum = '')" class="botonIconCancelar">No</button>
           </div>
