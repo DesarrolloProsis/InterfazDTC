40e606686e8950f712ff5bda8e61df4918d98f76
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-03-18 15:08:42 -0600

    Btn Subir Escaneado

--- src/components/Header/CrearHeaderCalendario.vue
+++ src/components/Header/CrearHeaderCalendario.vue
@@ -51,12 +51,10 @@
                                     <button @click="escaneadoBool = false, calendar_escaneado = ''" class="botonIconCancelar mt-2 ml-4 h-10 text-sm justify-center px-1">Cancelar</button>
                                 </div>                                
                             </div>                        
-                            <div v-else class=" justify-center">
-                                <input type="file" @change="recibir_calendario_escaneado" class="opacity-0 w-12 h-12 absolute" multiple/>
-                                <button @click="enviar_calendario_escaneado" class="botonIconCancelar">
+                            <div v-else class=" justify-center botonIconCancelar">
+                                <input type="file" @change="recibir_calendario_escaneado" class="opacity-0 w-12 h-12 absolute" multiple/>                   
                                     <img src="../../assets/img/pdf-sellado.png" class="mr-2" width="25" height="25" />
-                                    <span>Subir Escaneado</span>
-                                </button>                                       
+                                    <span>Subir Escaneado</span>                                                             
                             </div>
                         </div>
                     </div>
