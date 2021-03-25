b7a6d1bfc6d7c56697bad653c614527258f9a8ed
unknown <geoffreyytorres25@gmail.com>
2021-03-25 09:26:44 -0600

    VErsion 1.3.25 Beta

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,7 @@ npm run build<template>
                     </p>
                 </div>
                 <div>
-                    <p class="sm:text-xs text-sm sm:mt-1 text-blue-400">V1.3.24</p>
+                    <p class="sm:text-xs text-sm sm:mt-1 text-blue-400">V1.3.25</p>
                 </div>
             </div>                                
         </div>        
