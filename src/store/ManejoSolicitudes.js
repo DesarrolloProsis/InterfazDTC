0efb5c4a752325ffcdd8a0acc9a9bbbf286f0c6f
unknown <geoffreyytorres25@gmail.com>
2021-03-26 12:39:35 -0600

    Adelantos Interceptor

--- src/store/ManejoSolicitudes.js
+++ src/store/ManejoSolicitudes.js
@@ -1,9 +1,18 @@
 import Axios from 'axios'
 //import CookiesService from '../services/CookiesService'
+
 export default function manejor_solicitudes_run(){
-    Axios.interceptors.request.use((config) => {
-        //console.log(config.headers.Authorization )
-        //console.log(CookiesService.obtener_bearer_token())
+    Axios.interceptors.request.use((config) => {            
+        if(config.url.includes('login'))            
+            return config        
+        else{               
+            if(config.url.includes('squaresCatalog')){
+                console.log(config)
+            }
+            //config['header'] = CookiesService.obtener_bearer_token()
+            //console.log(CookiesService.obtener_bearer_token())
+        }
+        
         return config
     },(error) => {
         return Promise.reject(error)
