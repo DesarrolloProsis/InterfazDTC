b606a9bc624892a1786c9d6a7a400e8e07e7dddf
unknown <geoffreyytorres25@gmail.com>
2021-03-26 11:54:23 -0600

    Bug interceptor Validar Login

--- src/store/ManejoSolicitudes.js
+++ src/store/ManejoSolicitudes.js
@@ -1,9 +1,9 @@
 import Axios from 'axios'
-import CookiesService from '../services/CookiesService'
+//import CookiesService from '../services/CookiesService'
 export default function manejor_solicitudes_run(){
     Axios.interceptors.request.use((config) => {
-        console.log(config.headers.Authorization )
-        console.log(CookiesService.obtener_bearer_token())
+        //console.log(config.headers.Authorization )
+        //console.log(CookiesService.obtener_bearer_token())
         return config
     },(error) => {
         return Promise.reject(error)
