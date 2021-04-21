5e8b3460f72b67fe7fc86a857b0b7277787b02d6
unknown <geoffreyytorres25@gmail.com>
2021-04-21 09:36:02 -0500

    Test NavBar Store

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,31 +1,37 @@
-import { mount, createLocalVue } from '@vue/test-utils'
-import Vuex, { Store } from 'vuex'
+
+import Vuex from 'vuex'
 import NavBar from '../../src/components/Navbar.vue'
+import { mount, createLocalVue } from "@vue/test-utils"
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-const store = new Store({
-  modules:{
-      Login: {
-        namespaced: true,
-        state: {
-          cookiesUser:{
-            rollId: 1
-          }
+const store = new Vuex.Store({
+  modules:{    
+    Login: {
+      namespaced: true,
+      state: {
+        cookiesUser:{
+          rollId: "alice"
         }
       }
+    }
   }
 })
-
-test('HelloWorld.vue', () => {
-  const wrapper = mount(NavBar, {
-    propsData: {
-      pruebas: 'Hello world'
-    }
+describe("ComponentWithVuex", () => {
+  it("renders a username using a real Vuex store", () => {
+    const wrapper = mount(NavBar, { 
+      store, 
+      localVue 
+    })
+    
+    console.log(wrapper.find(".rollUsuario"))
+    
+    
   })
-  console.log(wrapper)
-  // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('Hello world')
-
 })
+
+
+
+
+
