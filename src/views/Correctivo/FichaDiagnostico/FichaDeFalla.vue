2169ab97108a4beefa1412ffb7254118de265e8d
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-04-26 18:36:17 -0500

    .

--- src/views/Correctivo/FichaDiagnostico/FichaDeFalla.vue
+++ src/views/Correctivo/FichaDiagnostico/FichaDeFalla.vue
@@ -72,6 +72,7 @@ export default {
     methods:{
         actualizar_header(header){
             this.datosHeader = header
+            console.log(header)
         },
         validar_horas(){
             let horaISplite = this.datosHeader.horaInicio.split(':')            
@@ -81,7 +82,8 @@ export default {
             return dateInicio < dateFin ? true : false                             
         },
         enviar_header_ficha(value){    
-            let llavesHeader = Object.keys(this.datosHeader)            
+            let llavesHeader = Object.keys(this.datosHeader)  
+            console.log(llavesHeader)          
             if(llavesHeader.length == 10){            
                 let valueHeader = Object.values(this.datosHeader)
                 let validar = valueHeader.some(prop => prop == '')            
