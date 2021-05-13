8a5cf9e5b64a1c7cde3bdb0939e58cbecba3721f
unknown <geoffreyytorres25@gmail.com>
2021-05-13 09:33:07 -0500

    Bug Plaza Usuarios

--- src/services/CookiesService.js
+++ src/services/CookiesService.js
@@ -31,9 +31,10 @@ function formato_cookies_usuario(loginSesion){
             localStorage.setItem('cookiesUser', JSON.stringify(cookies));  
             localStorage.setItem('token', JSON.stringify(tokenUser))  
             store.dispatch("Login/BUSCAR_PLAZAS");  
-            //store.commit("Login/LISTA_HEADER_PLAZA_USER_MUTATION", response.data.result.loginList) 
-            store.commit("Usuarios/Consulta_Users", )
-            store.commit("Header/LISTA_HEADERS_MUTATION", response.data.result.loginList);              
+            store.commit("Login/LISTA_HEADER_PLAZA_USER_MUTATION", response.data.result.loginList) 
+            store.dispatch("Usuarios/Consulta_Users", {id: cookies.userId, square: cookies.plazasUsuario[0].refereciaPlaza})
+            store.commit("Header/LISTA_HEADERS_MUTATION", response.data.result.loginList);      
+            this.actualizar_plaza()                    
             resolve(cookies)                 
         })                                                       
     })
