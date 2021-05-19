53440d76035e69bbd1f8c8a5d71d4fb09b89a94d
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-05-19 11:52:49 -0500

    botones Tec

--- src/views/Correctivo/Concentrados/ConcentradoDTC.vue
+++ src/views/Correctivo/Concentrados/ConcentradoDTC.vue
@@ -6,7 +6,7 @@
         ////                         MODAL CARRUSEL                        /////
         ////////////////////////////////////////////////////////////////////-->
         <div class="sticky inset-0">
-          <div v-if="carruselModal" class="rounded-lg border max-w-2xl h-69 justify-center absolute inset-x-0 bg-white mx-auto border-gray-400 shadow-2xl mt-48">          
+          <div v-if="carruselModal" class="carruselGMMEP">          
             <div class="justify-center text-center block">            
                 <Carrusel @cerrar-modal-carrusel="carruselModal = false, arrayImagenesCarrusel = []" :arrayImagenes="arrayImagenesCarrusel"></Carrusel>
             </div>
@@ -326,6 +326,7 @@ editar_status_dtc: function (){
       .catch(error => {                    
         console.log(error);
       });
+      this.lista_DTC_Filtrada = this.infoDTC
     }
     else {
           this.$notify.warning({
