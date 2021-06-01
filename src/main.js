7df21c9916e9d26beaca1351199342b48ebc9bd4
unknown <geoffreyytorres25@gmail.com>
2021-06-01 12:51:14 -0500

    Actualizacion de Vee-VAlitae

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import router from './router'
 import store from './store'
 import "./assets/css/tailwind.css"
 import Multiselect from 'vue-multiselect'
-import VeeValidate from 'vee-validate';
-import { Validator, ValidationObserver, ValidationProvider } from 'vee-validate';
-import es from "vee-validate/dist/locale/es";
+import { ValidationObserver, ValidationProvider, localize, extend  } from 'vee-validate';
+import es from 'vee-validate/dist/locale/es.json';
 import './registerServiceWorker'
 import moment from "moment";
+import * as rules from 'vee-validate/dist/rules';
 import VTooltip from 'v-tooltip'
 Vue.use(VTooltip)
 //Rputer
@@ -23,7 +23,10 @@ Vue.use(VueLazyImage)
 //Validaciones en formularios
 Vue.component('ValidationObserver', ValidationObserver);
 Vue.component('ValidationProvider', ValidationProvider);
-Vue.use(VeeValidate, { classes: false,}); 
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+localize('es', es);
 const NoSiniestro = {
   validate(value){
       return store.getters['Header/GET_UNIQUE_SINESTER_NUMBER'](value)
@@ -39,21 +42,18 @@ const FechaValidaList = {
       let fechas = value.split('\n')
       for(let item of fechas){
         if(item.length == 10){
-          if(!moment(item, 'YYYY/MM/DD').isValid()){
-            return false
-          }
+          if(!moment(item, 'YYYY/MM/DD').isValid())
+            return false          
         }
-        else{
-          return false
-        }
+        else
+          return false        
       }
       return true   
   }
 }
-Validator.extend('uniqueSinester', NoSiniestro)
-Validator.extend('uniqueReport', NoReporte)
-Validator.extend('FechaValidaList',FechaValidaList)
-Validator.localize("es",es);
+extend('uniqueSinester', NoSiniestro)
+extend('uniqueReport', NoReporte)
+extend('FechaValidaList',FechaValidaList)
 // Componente Multiselect
 Vue.component('multiselect', Multiselect)
 // Notificaciones 
@@ -62,6 +62,8 @@ Vue.use(VueNotify)
 //manejor de Solicitudes axios
 import AxiosManejador from './store/ManejoSolicitudes'
 Vue.prototype.$http = AxiosManejador
+//Vlidaciones
+
 Vue.config.productionTip = false
 new Vue({
   router,
