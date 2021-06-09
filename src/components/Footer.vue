20f7a8ac6e9ad9c8153eec58aa7c724d8ecdbc35
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-06-09 17:04:14 -0500

    V1.6.2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,7 @@
                     </p>
                 </div>
                 <div>
-                    <p class="sm:text-xs text-sm sm:mt-1 font-titulo font-light text-blue-400">V1.5.7</p>
+                    <p class="sm:text-xs text-sm sm:mt-1 font-titulo font-light text-blue-400">V1.6.2</p>
                 </div>
             </div>                                
         </div>        
