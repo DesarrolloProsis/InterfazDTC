8e719028b8d506605611edb102a72e9f225a4fe9
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-07-05 13:40:16 -0500

    .

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -31,6 +31,7 @@
                             <li>·Se puede realizar una busqueda por referencia de DTC en concentrado de Diagnóstico.</li>
                             <li>·Ahora el Tamaño del PDF con Imagenes es Más Pequeño.</li>
                             <li>·Ficha Técnica Por Fin de Vida Útil y Por Siniestro, Ocupa las Imágenes del Diagnóstico.</li>
+                            <li>·Acomodo de Botones.</li>
                         </ol>
                     </div>
                     <p class="sm:text-xs ml-5 text-sm sm:mt-1 font-titulo font-light text-blue-400">V1.6.6</p>
