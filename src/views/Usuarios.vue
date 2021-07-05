95d421a455978f36f480a1f77c9be88fc4819d4a
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-07-05 09:02:25 -0500

    Usuarios Bitacora

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -383,6 +383,7 @@ export default {
               width: 500,
             },
           });
+          this.refrescar_usuarios()  
           this.modal = false;
         }
         this.limpiar_usuario();      
