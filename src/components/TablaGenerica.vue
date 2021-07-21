03be7ca0d34d4f6d2c59b8c596d65a21e46b99f2
unknown <geoffreyytorres25@gmail.com>
2021-07-21 12:13:44 -0500

    Control de moreInformation movil

--- src/components/TablaGenerica.vue
+++ src/components/TablaGenerica.vue
@@ -7,27 +7,16 @@
             ////////////////////////////////////////////////////////////////////-->
             <thead id="">
                 <tr class="text-md text-gray-400 font-normal bg-blue-800 sm:hidden md:hidden">
-                    <th v-for="(item, key) in normalheaderKey" :key="key" class="cabeceraTable sm:text-xs">{{ item }}</th>
+                    <th v-for="(item, keyCompu) in normalheaderKey" :key="keyCompu" class="cabeceraTable sm:text-xs">{{ item }}</th>
                 </tr>
                  <tr class="text-md text-gray-400 font-normal bg-red-800 lg:hidden xl:hidden">
-                    <th v-for="(item, key) in movilHeaderKey" :key="key" class="cabeceraTable sm:text-xs">{{ item }}</th>
+                    <th v-for="(item, keyMovil) in movilHeaderKey" :key="keyMovil" class="cabeceraTable sm:text-xs">{{ item }}</th>
                 </tr>
-                <!-- <tr  class="text-md text-gray-400 font-normal bg-blue-800">  
-
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Referencia</th>
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Plaza</th>
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Fecha</th>
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Ubicacion</th>
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Folio de Falla</th>
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Folio de Siniestro</th>                                                                                                
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Referencia DTC</th>
-                    <th class="py-2 px-3 top-0 border-b border-gray-200 bg-blue-800 font-medium">Acciones</th>
-                </tr> -->
             </thead>
             <!--/////////////////////////////////////////////////////////////////
             ////                          BODY TABLA                          ////
             ////////////////////////////////////////////////////////////////////-->
-            <tbody name="table" id="multiselectHamburguesa">
+            <tbody id="filaTablaMovil" name="table">
                 <template v-if="lista.length == 0 && loadingTabla != true"> 
                     <tr>
                         <td class="w-full text-center text-red-500 m-10" colspan="9">                                    
@@ -43,31 +32,24 @@
                     </tr>  
                 </template>                            
                 <template v-if="lista.length > 0">
-                    <tr v-for="(item, key) in lista" :key="key" class="h-12 text-gray-900 text-sm text-center">                
-                        <td class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item.referenceNumber }}</td>
-                        <td class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item.squareName }}</td>
-                        <td class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item.diagnosisDate.slice(0,10) | formato_concentrado }}</td>
-                        <td class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item.lanes }}</td>
-                        <td class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item.failuerNumber }}</td>
-                        <td class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item.siniesterNumber }}</td>                              
-                        <td class="border-dashed border-t border-gray-200 px-3">{{ item.referenceDTC }}</td>
-                        <td class="border-dashed border-t border-gray-200 px-3">                                               
-                            <!-- <multiselect v-model="value" @close="acciones_mapper(item)" placeholder="Seleccione una Accion" label="title" track-by="title" :options="opticones_select_acciones(item)" :option-height="200" :custom-label="customLabel" :show-labels="false">
-                                <template slot="singleLabel" slot-scope="props" class="static">
-                                    <div class="inline-flex">
-                                        <img :src="props.option.img" class="mr-5" width="15" height="15">                                                               
-                                        <span class="option__title bg-red-300">{{ props.option.title }}</span>
-                                    </div>
-                                </template>
-                                <template slot="option" slot-scope="props" class="static">                                                
-                                    <div class="option__desc "><span class="option__title inline-flex">
-                                        <img :src="props.option.img" class="mr-5" width="15" height="15">    
-                                        {{ props.option.title }}</span>
-                                    </div>
-                                </template>
-                            </multiselect>                                                                                                                                                                                 -->
-                        </td>
+                    <tr v-for="(item, keyCompu) in lista" :key="'C' + keyCompu" class="h-12 text-gray-900 text-sm text-center sm:hidden md:hidden">                                        
+                        <td v-for="(itemSub, keyCompuSub) in normalheaderKey" :key="'CS' +keyCompuSub " class="border-dashed border-t border-gray-200 px-3 sm:text-xs">{{ item[itemSub] }}</td>                      
                     </tr>
+                    <tr v-for="(item, keyMovil) in lista" :key="'M' + keyMovil" class="h-12 text-gray-900 text-sm text-center lg:hidden xl:hidden 4k:hidden">                                        
+                        <td v-for="(itemSub, keyMovilSub) in movilHeaderKey" :key="'MS' + keyMovilSub" class="border-dashed border-t border-gray-200 px-3 sm:text-xs">
+                            <div v-if="itemSub != 'Acciones'">
+                                {{ item[itemSub] }}
+                            </div>
+                            <div v-else>
+                                <button v-if="item.moreInformationMovil == false" @click="insertar_more_infomacion(keyMovil)" :disabled="item.disableMoreInformation">
+                                    <img src="../assets/img/flecha-hacia-abajo.png" class="w-5 h-5" alt="">                                    
+                                </button>
+                                 <button v-else @click="delete_more_information(keyMovil)">                                    
+                                    <img src="../assets/img/flecha-hacia-arriba.png" class="w-5 h-5" alt="">
+                                </button>
+                            </div>                           
+                        </td>                        
+                    </tr>                   
                 </template>  
             </tbody>
         </table>
@@ -84,8 +66,8 @@ export default {
             lista: [],
             loadingTabla: false,
             windowWidth: '',
-            normalheaderKey: ['referenceNumber', 'squareName', 'squareName', 'diagnosisDate', 'lanes', 'failuerNumber', 'siniesterNumber', 'referenceDTC'],
-            movilHeaderKey: ['referenceNumber', 'diagnosisDate']
+            normalheaderKey: ['referenceNumber', 'squareName', 'squareName', 'diagnosisDate', 'lanes', 'failuerNumber', 'siniesterNumber', 'referenceDTC', 'Acciones'],
+            movilHeaderKey: ['referenceNumber', 'diagnosisDate', 'Acciones']
         }
     },
     mounted() {
@@ -112,48 +94,45 @@ export default {
         this.$http.get(`${API}/diagnosticoFalla/GetBitacoras/TLA/${userId}`)
         .then((response) => { 
             console.log(response)           
-            this.lista = response.data.result          
+            this.lista = response.data.result.map(item => {
+                item['moreInformationMovil'] = false
+                item['disableMoreInformation'] = false
+                return item
+            })          
         })
         .catch(() => {                                                        
         })                       
     },
     methods: {
-        size_screen(){
-                          if(window.innerWidth >= 320 && window.innerWidth < 895){
-                this.windowWidth = 'sm'   
-            }
-            else if(window.innerWidth >= 895 && window.innerWidth < 1024){
-                this.windowWidth = 'md'
-            }
-            else if(window.innerWidth >= 1024 && window.innerWidth < 1279){
-                this.windowWidth = 'lg'
-            }
-            else if(window.innerWidth >= 1279 && window.innerWidth < 2040){
-                this.windowWidth = 'xl'
-            }
-            else{
-                this.windowWidth = '4k'
-            }   
+        insertar_more_infomacion(index){
+            this.disableFilasMovil(true, index)        
+            index += this.lista.length   
+            let nodo = document.getElementById('filaTablaMovil')                     
+            let nodoBefore = nodo.children[index + 1]              
+            let nodoNew = document.createElement('tr')                                  
+            nodoNew.innerHTML = `
+                <td colspan="${this.movilHeaderKey.length}" class="bg-blue-600">hola mundo</td>                
+            `            
+            nodo.insertBefore(nodoNew, nodoBefore)
+        },
+        delete_more_information(index){
+            this.disableFilasMovil(false, index)                                    
+            index = index + this.lista.length
+            let nodo = document.getElementById('filaTablaMovil')
+            let nodoDelete = nodo.children[index + 1]            
+            nodo.removeChild(nodoDelete)
+        },
+        disableFilasMovil(statusChange, index){
+            this.lista.forEach((item, indexfor, listafor) => {
+                let updateObj = Object.assign(item)       
+                if(indexfor == index)                   
+                    updateObj['moreInformationMovil'] = statusChange
+                if(indexfor != index)
+                    updateObj['disableMoreInformation'] = statusChange
+                listafor.slice(index, 1, updateObj)   
+            })
         }
-
     },           
-    watch:{
-        windowWidth(newSize){
-            if(newSize == 'sm' || newSize == 'md'){
-                console.log('mini')
-            }
-            else {
-                let tableHeader = window.document.getElementById('headerTab')
-                // tableHeader.innerHTML = `
-                //     <tr v-for="(item, key) in normalheaderKey" :key="key" class="text-md text-gray-400 font-normal bg-blue-800">
-                //         <td class="cuerpoTable sm:text-xs">{{ item }}</td>
-                //     </tr>
-                // `
-                console.log(tableHeader)
-            }
-        }
-    }
-
 }
 </script>
 
