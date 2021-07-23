666350b5a38164d659fe9b13bf1b54861e8b50bc
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-07-23 11:24:32 -0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     ],
     screens:{
       'sm': {'min': '320px', 'max': '765px'},
-      'md': {'min': '766YNpx', 'max': '1024px'},
+      'md': {'min': '766px', 'max': '1024px'},
       'lg': {'min': '1025px', 'max': '1279px'},
       'xl': {'min': '1280px', 'max':'2040px'},
       '4k': {'min': '2040px', 'max':'3840px'}
