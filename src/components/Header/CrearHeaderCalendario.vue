349711c415bf95b0cc7f56f896b9e6f9a2483090
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-07-23 18:32:11 -0500

    nuevo calendario

--- src/components/Header/CrearHeaderCalendario.vue
+++ src/components/Header/CrearHeaderCalendario.vue
@@ -4,124 +4,85 @@
           ////                    Modal Escaneado                          ////
           ////////////////////////////////////////////////////////////////////-->
         <PdfEscaneado @limpiar-componente-escaneado="limpiar_componete_escaneado" :abrirModal="modalSubirSellado" :objInsert="objInsertEscaneado" :tipoReporte="'Calendario'"></PdfEscaneado>
-        <!--///////////////////////////////////////////////////////////////////
-          ////                             HEADER                          ////
-          ////////////////////////////////////////////////////////////////////-->
-        <div class="">
-            <div class="text-2xl text-center inline-flex sm:inline-block w-full mt-1 sm:w-67">
-                <div class="w-auto mx-auto justify-center sm:ml-16 sm:-mb-6 ">
-                    <div class="w-66 p-5">
-                        <img src="../../assets/img/prosis-logo.jpg" class="h-12 w-48"/>
+        <div class="felx-auto ml-2 mr-2 mt-2 -mb-6">
+            <div class="grid grid-cols-1 xl:grid-cols-2 sm:grid-cols-1 sm:px-2">
+                <!--Logo-->
+                <div class="flex mx-auto w-auto m-2 sm:grid-cols-1">    
+                    <img src="../../assets/img/prosis-logo.jpg" class="h-12 w-auto m-2"/>
+                </div>
+                <!--Titulo-->
+                <div class="flex font-titulo font-bold mx-auto">
+                    <h1 class="mx-auto my-auto w-auto text-center text-4xl sm:text-sm">Calendario de Actividades</h1>
+                </div>
+                <!--Mes y Acciones-->
+                <div class="grid grid-cols-1 mx-auto ">
+                    <div class="flex m-3">
+                        <p class="font-titulo font-semibold">Correspondiente al mes de:</p>
+                        <h2 class="mx-auto font-titulo">{{ `${mesNombre} del ${año}` }}</h2>
+                    </div>
+                    <div class="flex m-3 h-12 sm:grid sm:grid-cols-1 sm:mx-auto sm:-mt-2">
+                        <p class="font-titulo font-semibold sm:text-center">Acciones:</p>
+                        <multiselect v-model="value" @close="acciones_mapper()" class="w-68 sm:w-65" placeholder="Seleccione una Accion" label="title" track-by="title" :options="opticones_select_acciones()" :option-height="100" :custom-label="customLabel" :show-labels="false">
+                            <template slot="singleLabel" slot-scope="props">
+                                <div class=" inline-flex">
+                                    <img :src="props.option.img" class="mr-5" width="15" height="15">                                                               
+                                    <span class="option__title">{{ props.option.title }}</span>
+                                </div>
+                            </template>
+                            <template slot="option" slot-scope="props">                                                
+                                <div class="option__desc"><span class="option__title inline-flex">
+                                    <img :src="props.option.img" class="mr-5" width="15" height="15">    
+                                    {{ props.option.title }}</span>
+                                </div>
+                            </template>
+                        </multiselect>
                     </div>
                 </div>
-                <div class="w-2/3 sm:w-auto sm:mt-3 sm:text-sm sm:-mb-6 md:-ml-18 flex justify-start sm:justify-center">
-                    <div class="border-gray-200 p-5 pt-0 ml-4 sm:ml-0">
-                        <h1 class="mt-5 border-purple-800 w-72 sm:w-full font-titulo font-bold">{{ `Calendario de Actividades` }}</h1>            
+                <!--Plaza y Codigo de Colores-->
+                <div class="grid grid-cols-1 mx-auto">
+                    <div class="flex m-3 sm:grid sm:grid-cols-1">
+                        <span class="font-titulo font-semibold mx-auto">Plaza/Encargado: </span>
+                        <p class="sm:ml-2">
+                            <SelectPlaza @actualizar-plaza="cambiar_plaza" :vista="'Calendario'" :fullPlazas="true" :tipo="'tipoPlazaSelect'">
+                            </SelectPlaza>
+                        </p>
                     </div>
-                </div>
-            </div>
-            <div class="inline-flex md:flex-col sm:inline-block -mb-3 ml-1 w-full h-full sm:w-67 sm:-ml-1 md:grid-cols-2 md:ml-2">
-                <!--///////////////////////////////////////////////////////////////////
-                  ////                     COLUMNA IZQUIERDA                        ////
-                  ////////////////////////////////////////////////////////////////////-->
-                <div class="w-full sm:w-full inline-flex sm:flex-col text-base sm:text-xs md:text-sm md:flex-col md:ml-3 ">
-                    <div class="w-1/2 h-62 grid grid-cols-1 sm:w-67 sm:h-48 md:w-full md:h-48 md:grid-cols-2 md:-ml-2 p-1 sm:p-2 -mb-12">
-                        <div class="flex mt-10 ml-5 -mb-10 sm:-mt-1 sm:p-1 sm:ml-2 sm:-mb-10 w-full sm:w-66 sm:h-6 md:w-auto md:ml-0 md:h-10 md:mt-1">
-                            <p class="font-titulo sm:ml-1 font-semibold md:ml-1 md:mt-1">Correspondiente al mes de:</p>
-                            <h2 class="ml-5 sm:ml-16 font-titulo md:ml-8 md:mt-1  md:mb-3">{{ `${mesNombre} del ${año}` }}</h2>
-                        </div>                    
-                        <div class="-mb-6 -mt-6 md:grid-cols-2 lg:flex sm:w-66 grid grid-cols-1 justify-start sm:ml-2 sm:p-1 sm:mb-1 sm:mt-8 md:ml-1 ml-5 md:-mt-1">
-                            <span class="font-titulo mt-1 font-semibold mr-4 sm:-mb-4 sm:ml-1 md:mt-2 md:h-10">Plaza/Encargado: </span>
-                            <p class="ml-18  -mt-20 sm:ml-1 sm:mt-4 md:mt-1 md:-ml-32 md:h-10"><SelectPlaza @actualizar-plaza="cambiar_plaza" :vista="'Calendario'" :fullPlazas="true" :tipo="'tipoPlazaSelect'"></SelectPlaza></p>
-                        </div>
-                        <div class="grid grid-cols-1 sm:w-66 sm:p-1 sm:mb-8 ml-5 -mt-16 sm:mt-4 sm:ml-2 md:ml-1 md:-mt-1 md:h-10">
-                            <span class="font-titulo font-semibold mr-4 sm:-mb-4 sm:ml-1 md:mt-2">Acciones:</span>
-                            <multiselect v-model="value" @close="acciones_mapper()" class="-mt-24 ml-33 sm:ml-1 sm:w-full sm:mt-4 md:-mt-8 md:mb-10 md:ml-32 w-36" placeholder="Seleccione una Accion" label="title" track-by="title" :options="opticones_select_acciones()" :option-height="200" :custom-label="customLabel" :show-labels="false">
-                                <template slot="singleLabel" slot-scope="props">
-                                    <div class=" inline-flex">
-                                        <img :src="props.option.img" class="mr-5" width="15" height="15">                                                               
-                                        <span class="option__title">{{ props.option.title }}</span>
-                                    </div>
-                                </template>
-                                <template slot="option" slot-scope="props">                                                
-                                    <div class="option__desc"><span class="option__title inline-flex">
-                                        <img :src="props.option.img" class="mr-5" width="15" height="15">    
-                                        {{ props.option.title }}</span>
-                                    </div>
-                                </template>
-                            </multiselect>             
-                        </div>
-                        <div class="sm:hidden lg:hidden xl:hidden h-10 md:grid-cols-2 grid justify-start md:ml-1 md:-mt-1">
-                            <span class="font-titulo mt-1 font-semibold mr-4 sm:-mb-4 sm:ml-1 md:mt-2">Código de Colores: </span>
-                            <p class="ml-18 -mt-20 sm:ml-1 sm:mt-4 md:mt-1 md:-ml-32">
-                                <select class="ml-20 w-48 is_valid">
+                    <div class="flex m-3 sm:grid sm:grid-cols-1 sm:mx-auto sm:-mt-2">
+                        <span class="font-titulo font-semibold sm:mx-auto">Código de Colores: </span>
+                            <p class="">
+                                <select class="ml-20 w-48 is_valid sm:w-62 sm:ml-5">
                                     <option value="">Códgio de Colores</option>
-                                    <option class="bg-green-500" value="1">Semanal</option>
-                                    <option class="bg-red-500" value="2">Mensual</option>
-                                    <option class="bg-blue-500" value="3">Trimestral</option>
-                                    <option class="bg-pink-500" value="4">Semestral</option>
-                                    <option class="bg-orange-500" value="4">Anual</option>
+                                    <option class="bg-green-500 text-center text-gray-200" value="1">Semanal</option>
+                                    <option class="bg-red-500 text-center text-gray-200" value="2">Mensual</option>
+                                    <option class="bg-blue-500 text-center text-gray-200" value="3">Trimestral</option>
+                                    <option class="bg-pink-500 text-center text-gray-200" value="4">Semestral</option>
+                                    <option class="bg-orange-500 text-center text-gray-200" value="4">Anual</option>
                                 </select>
                             </p>
-                        </div>
                     </div>
-                    <div class="w-1/2 sm:mt-12 sm:ml-0 sm:w-67 sm:h-48 md:w-full md:-ml-2">
-                    <ValidationObserver ref="observer" class="-ml-16">  
-                        <div class="w-auto -mt-4 ml-20 sm:ml-5 sm:-mt-6 sm:w-66 p-8 sm:p-1 md:p-1 md:ml-1 md:w-32">
-                            <ValidationProvider name="ComentarioCalendario" rules="required:max:500"  v-slot="{ errors }">
-                                <span class="text-center font-titulo font-semibold text-gray-800 sm:ml-0">Observaciones</span>          
+                </div>
+                <!-- Observaciones-->
+                <div class="grid grid-cols-1 col-span-2 hidden">
+                    <ValidationObserver ref="observer" class="">  
+                        <div class="w-auto xl:ml-56 xl:mr-61  mx-auto">
+                            <ValidationProvider name="ComentarioCalendario" rules="required:max:500" v-slot="{ errors }">
+                                <span class="text-center font-titulo font-semibold text-gray-800">Observaciones</span>          
                                 <textarea
                                     v-model="comentario"                                                               
-                                    class="block container placeholder-gray-500 sm:mt-2 sm:mb-1 textAreaCalendario mt-6 md:w-76 md:ml-5 md:mt-4"
+                                    class="block container placeholder-gray-500 textAreaCalendario mt-3 ml-13"
                                     placeholder="jane@example.com"
                                     name="ComentarioCalendario"
                                     :maxlength="limite"
                                 />
                                 <span class="text-red-600 text-xs block">{{ errors[0] }}</span>
-                                <span class="text-xs text-gray-500 sm:ml-33 md:ml-66 ">{{ restante }}/500</span>    
+                                <span class="text-xs text-gray-500 ml-76">{{ restante }}/500</span>    
                             </ValidationProvider>                    
                         </div>  
-                    </ValidationObserver>            
-                    </div>
-                </div>
-                <!--///////////////////////////////////////////////////////////////////////
-                  ////                     CODIGO DE COLORES                          ////
-                  ////////////////////////////////////////////////////////////////////-->
-                <div class="w-1/2 ml-16 sm:ml-0 sm:w-67 uppercase text-xs p-10 sm:p-5 md:hidden">
-                    <p class="font-titulo font-semibold sm:ml-2">Codigo de Colores:</p>
-                    <div class="flex justify-between mt-2 ml-2">
-                        <p class="font-titulo">Semanal</p>
-                        <div class="bg-green-500 text-gray-200 border-gray-800 border w-16 h-6 ml-3 text-center rounded-xl border-none -mt-1">
-                            <p class="mt-1 font-titulo">Verde</p>
-                        </div>
-                    </div>
-                    <div class="flex justify-between mt-2 ml-2">
-                        <p class="font-titulo">Mensual</p>
-                        <div class="bg-red-500 text-gray-200 border-gray-800 border w-16 h-6 ml-3 text-center rounded-xl border-none -mt-1">
-                            <p class="mt-1 font-titulo">Rojo</p>
-                        </div>
-                    </div>
-                    <div class="flex justify-between mt-2 ml-2">
-                        <p class="font-titulo">Trimestral</p>
-                        <div class="bg-blue-500 text-gray-200 border-gray-800 border w-16 h-6 ml-3 text-center rounded-xl border-none -mt-1">
-                            <p class="mt-1 font-titulo">Azul</p>
-                        </div>
-                    </div>
-                    <div class="flex justify-between mt-2 ml-2">
-                        <p class="font-titulo">Semestral</p>
-                        <div class="bg-pink-500 text-gray-200 border-gray-800 border w-16 h-6 ml-3 text-center rounded-xl border-none -mt-1">
-                            <p class="mt-1 font-titulo">Rosa</p>
-                        </div>
-                    </div>
-                    <div class="flex justify-between mt-2 ml-2">
-                        <p class="font-titulo">Anual</p>
-                        <div class="bg-orange-500 text-gray-200 border-gray-800 border w-16 h-6 ml-3 text-center rounded-xl border-none -mt-1">
-                            <p class="mt-1 font-titulo">NARANJA</p>
-                        </div>
-                    </div>
+                    </ValidationObserver>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
