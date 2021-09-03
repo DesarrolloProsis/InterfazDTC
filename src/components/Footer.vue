f884a7c4c4642f37c00feb12ae54b1b0dbeec3ab
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-09-03 14:28:37 -0500

    V2.0.3 Cambios en Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,11 @@
                     <div ref="tooltipRef" class=" font-titulo">
                         <p class="text-center text-gray-800">🎉 Nueva Versión 2.0.3 !!! 🎉</p>
                         <ol class="pl-5 mt-2 text-gray-800 pb-2">
-                            <li>·Se puede seleccionar distinto encargado de plaza en Reporte de Mantenimiento Preventivo.</li>
+                            <li>·En concentrado DTC se muestra el usuario asignado de cada DTC.</li>
+                            <li>·Codigo de colores en concentrado DTC.</li>
+                            <li>·Se agregó un loop de carril más en cada plaza.</li>
+                            <li>·Nuevo formato de cambio de contraseña e información de Técnico.</li>
+                            <li>·Iconos de ayuda al crear un nuevo Diagnóstico y Ficha Técnica.</li>
                         </ol>
                     </div>                    
                 </div>
