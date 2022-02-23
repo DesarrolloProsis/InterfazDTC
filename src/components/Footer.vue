1ff0a4db073fd702cebb719c915794a2e1aad7ed
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-02-23 16:50:29 -0600

    version del footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -30,7 +30,7 @@
                     </div>                    
                 </div>
                 <div>
-                    <p class="sm:text-xs ml-5 text-sm sm:mt-1 font-titulo font-light text-blue-400">V2.0.5</p>
+                    <p class="sm:text-xs ml-5 text-sm sm:mt-1 font-titulo font-light text-blue-400">V2.0.6</p>
                 </div>
             </div>                                
         </div>        
